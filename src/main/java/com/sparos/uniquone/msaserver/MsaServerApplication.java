8f40fd90cf23bcd76c009910c3db688719d16afd
Seong Beom <bum090877@gmail.com>
2022-09-21 16:17:29 +0900

    first commit

---
+++ src/main/java/com/sparos/uniquone/msaserver/MsaServerApplication.java
@@ -0,0 +1,15 @@
+package com.sparos.uniquone.msaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MsaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparos/uniquone/msaserver/MsaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparos.uniquone.msaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
